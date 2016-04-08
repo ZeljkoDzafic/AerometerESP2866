038c731b9a3fce802d0b62a0742fe4553b55e44a
Zeljko Dzafic <zeljkodzafic@gmail.com>
2016-04-08 19:52:22 +0200

    Initi
    Init

---
+++ init.lua
@@ -0,0 +1,19 @@
+ --init.lua
+ wifi.setmode(wifi.STATION)
+ wifi.sta.config("Bourbon","1bourbon1")
+ wifi.sta.connect()
+ tmr.alarm(1, 1000, 1, function()
+  if wifi.sta.getip()== nil then
+  print("IP unavaiable, Waiting...")
+ else
+  tmr.stop(1)
+ print("ESP8266 mode is: " .. wifi.getmode())
+ print("The module MAC address is: " .. wifi.ap.getmac())
+ print("Config done, IP is "..wifi.sta.getip())
+ dofile ("aerometer.lua")
+ end
+ end)	
+ 
+ 
+
+
